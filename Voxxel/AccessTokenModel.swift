b9b0be83146326257624c7607154136017eb39a8
David Conner <dconner.pro@gmail.com>
2015-06-11 01:17:39 -0400

    added accesstoken files

---
+++ Voxxel/AccessTokenModel.swift
@@ -0,0 +1,14 @@
+//
+// Created by David Conner on 6/11/15.
+// Copyright (c) 2015 Voxxel. All rights reserved.
+//
+
+import Foundation
+
+class AccessTokenModel : NSObject {
+    var token:String
+    var tokenType:String
+    var client:String
+    var expiry:String
+    var uid:String
+}
