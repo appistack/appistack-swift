55e712bd8b7e97c4e2d358b5e3d4c102d14b2466
David Conner <dconner.pro@gmail.com>
2015-06-24 02:46:06 -0400

    Added ApiHelper
    * extended Alamofire to serialize JSON to objects

---
+++ Voxxel/ApiHelper.swift
@@ -0,0 +1,31 @@
+//
+//  ApiHelper.swift
+//  Voxxel
+//
+//  Created by David Conner on 6/24/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Alamofire
+
+@objc public protocol ResponseObjectSerializable {
+    init(response: NSHTTPURLResponse, json: AnyObject)
+}
+
+extension Alamofire.Request {
+    public func responseObject<T: ResponseObjectSerializable>(completionHandler: (NSURLRequest?, NSHTTPURLResponse?, T?, NSError?) -> Void) -> Self {
+        let serializer: Serializer = { (req, res, data) in
+            let JSONSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
+            let (json, serializationError):(AnyObject?, NSError?) = JSONSerializer(req, res, data)
+            if res != nil && json != nil {
+                return (T(response: res!, json: json!), nil)
+            } else {
+                return (nil, serializationError)
+            }
+        }
+        
+        return response(serializer: serializer, completionHandler: { (req, res, obj, error) in
+            completionHandler(req, res, obj as? T, error)
+        })
+    }
+}
