5f68976d59964474dd15b64e442fd7b3016721e7
David Conner <dconner.pro@gmail.com>
2015-07-12 03:18:01 -0400

    added SettingsViewController

---
+++ Voxxel/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Voxxel
+//
+//  Created by David Conner on 7/12/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
