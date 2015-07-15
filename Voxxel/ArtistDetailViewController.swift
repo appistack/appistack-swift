5e0c4953b2d541709ae336504568e4c9d6fd2cf9
David Conner <dconner.pro@gmail.com>
2015-07-15 11:55:54 -0400

    created ArtistDetailViewController

---
+++ Voxxel/ArtistDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ArtistDetailViewController.swift
+//  Voxxel
+//
+//  Created by David Conner on 7/15/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtistDetailViewController: UIViewController {
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
