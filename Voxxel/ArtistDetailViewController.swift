727adebb7b1f15a89e92b21f0a189935150e8349
David Conner <dconner.pro@gmail.com>
2015-07-19 15:23:23 -0400

    implement collectionView methods for sound cell
    * sounds aren't displaying bc ArtistList api request doesn't include them

--- Voxxel/ArtistDetailViewController.swift
+++ Voxxel/ArtistDetailViewController.swift
@@ -9,11 +9,17 @@
 import Foundation
 import UIKit
 
-class ArtistDetailViewController: UIViewController {
+class ArtistDetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    let artistService = ArtistService()
     var artist: Artist?
+    var sounds = [Sound]()
     
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var lblArtistName: UILabel!
     @IBOutlet weak var imgArtist: UIImageView!
+    
+    @IBOutlet weak var lblSoundCount: UILabel!
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -23,5 +29,46 @@ class ArtistDetailViewController: UIViewController {
         super.viewDidLoad()
         imgArtist.image = artist?.photo
         lblArtistName.text = artist?.name()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        collectionView.alwaysBounceVertical = true
+        
+        sounds = artist?.sounds ?? []
+        lblSoundCount.text = "\(sounds.count) Sounds"
+        registerNibs()
     }
+    
+    func registerNibs() {
+        let artistNib = UINib(nibName: "SoundCell", bundle: nil)
+        collectionView.registerNib(artistNib, forCellWithReuseIdentifier: "soundCell")
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("soundCell", forIndexPath: indexPath) as! SoundCell
+        
+        let sound = sounds[indexPath.item]
+        cell.lblName.text = sound.name
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // TODO: user selects sound
+    }
+}
+
+class SoundCell: UICollectionViewCell {
+    @IBOutlet weak var lblName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
 }
