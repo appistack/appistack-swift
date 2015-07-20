9db23044325f122e9e1b9fef96b513495b573c3b
David Conner <dconner.pro@gmail.com>
2015-07-19 21:04:20 -0400

    Sound Detail Ctrl: added basic controller

---
+++ Voxxel/SoundDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SoundDetailViewController.swift
+//  Voxxel
+//
+//  Created by David Conner on 7/19/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SoundDetailViewController: UIViewController {
+    var soundService = SoundService()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
