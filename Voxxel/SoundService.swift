874725418881c985a742751eb53f7fd11e04ac38
David Conner <dconner.pro@gmail.com>
2015-07-19 21:03:26 -0400

    SoundService: added basic api service to get sounds

---
+++ Voxxel/SoundService.swift
@@ -0,0 +1,19 @@
+//
+//  SoundService.swift
+//  Voxxel
+//
+//  Created by David Conner on 7/19/15.
+//  Copyright © 2015 Voxxel. All rights reserved.
+//
+
+import Foundation
+
+class SoundService {
+    let apiUrl = Config.conf.opts["api_base_url"]! + "/api/v1"
+    
+    func get(id:Int, completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Sound?, NSError?) -> Void) {
+        VoxxelApi.manager.request(.GET, apiUrl + "/sounds/\( id ).json")
+            .responseObject(completionHandler)
+    }
+    
+}
